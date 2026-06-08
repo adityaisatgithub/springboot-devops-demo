1b84d5c5161f26e9750224bc39c64b6ffebe1c04
Aditya Kumar <adityaicd2016@gmail.com>
2026-06-08 16:26:21 +0530

    Add environment variable demo

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${APP_NAME:DefaultApp}")
+    private String appName;
+
+    @GetMapping("/config")
+    public String config() {
+        return "Application Name: " + appName;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot Application is Running";
+    }
+}

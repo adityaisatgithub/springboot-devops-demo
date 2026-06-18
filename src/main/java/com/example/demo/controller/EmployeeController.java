1c5665791b3b9e12e7c63c6826bdecb0b615698a
Aditya Kumar <adityaicd2016@gmail.com>
2026-06-18 10:11:08 +0530

    Added log

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -29,6 +29,7 @@ public class EmployeeController {
 
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee employee) {
+        System.out.println(employee);
         return employeeRepository.save(employee);
     }
 }
